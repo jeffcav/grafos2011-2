181570d042b558da3d1f7495bed8c8b3ce717629
felipe.ja.maia <felipe.ja.maia@8503d277-4d3b-60eb-4296-69b3f58a7824>
2011-09-30 02:53:24 +0000

    Agora a função atualizar_status() faz um desenho, no lugar de imprimir o grafo na tela. Através de mouse events, agora é possivel mover os nós.

--- estrutura.js
+++ estrutura.js
@@ -1,11 +1,68 @@
 var grafos = new Array();
 var numVertices = 0;
+var action = 0;
+var actNode = 9999;
 
-function novo_vertice(nome){
+window.addEventListener("load", atualizar_status, false);
+window.addEventListener("dblclick", onMouseDblClick, false);
+window.addEventListener("mousedown", onMouseDown, false);
+window.addEventListener("mouseup", onMouseUp, false);
+window.addEventListener("mousemove", onMouseMove, false);
+
+function novo_vertice(nome, x, y){
 	this.nome=nome;
 	this.linkpara = new Array();
 	this.numlinks = 0;
+	this.posx = x;
+	this.posy = y;
 }
+
+function onMouseDblClick(e){ // o ideal seria que o inserir vertices chamasse essa função que 
+	if( e.clientX > 10 && e.clientX < 510 && e.clientY < 602 && e.clientY > 102 ){
+		if(action == 1){ //inserir
+			grafos[numVertices] = new novo_vertice(numVertices, e.clientX, e.clientY);
+			numVertices++;
+			action = 0;
+			atualizar_status();
+		}else if(action == 2){ //remover
+			for(j=0; j < numVertices; j++){
+				if( e.clientX > grafos[j].posx - 20 && e.clientX < grafos[j].posx + 20 && e.clientY > grafos[j].posy - 20 && e.clientY < grafos[j].posy + 20 ){
+					//numVertices--;
+					action = 0;
+					grafos[j].nome = "apagado";
+					atualizar_status();
+				}	
+			}
+		}
+	}
+}
+
+function onMouseDown(e){ // o ideal seria que o inserir vertices chamasse essa função que 
+	if( e.clientX > 10 && e.clientX < 510 && e.clientY < 602 && e.clientY > 102 ){
+		if(action == 0){ //inserir
+			for(j=0; j < numVertices; j++){
+				if( e.clientX > grafos[j].posx - 20 && e.clientX < grafos[j].posx + 20 && e.clientY > grafos[j].posy - 20 && e.clientY < grafos[j].posy + 20 ){
+					actNode = j;
+				}	
+			}
+		}
+	}
+}
+
+function onMouseMove(e){ // o ideal seria que o inserir vertices chamasse essa função que 
+	if(action == 0 && actNode < 9999 ){
+		grafos[actNode].posx = e.clientX;
+		grafos[actNode].posy = e.clientY;
+		atualizar_status();
+	}
+}
+
+function onMouseUp(e){ // o ideal seria que o inserir vertices chamasse essa função que 
+	if(action == 0 && actNode < 9999 ){
+		actNode = 9999;
+	}
+}
+
 
 function inserir_link(){
 	var origem = document.getElementById("linkvertice1text").value;
@@ -18,28 +75,49 @@ function inserir_link(){
 	atualizar_status();
 }
 
+
+
 function inserir_vertice(){
-	grafos[numVertices] = new novo_vertice(numVertices);
-	numVertices++;
-	
-	atualizar_status();
+	action = 1;
 }
 
 function atualizar_status(){
-	document.getElementById("statusdiv").innerHTML = "";
+	//document.getElementById("statusdiv").innerHTML = "";
+	var context = document.getElementById("area").getContext("2d");
+	//context.beginPath();
+	context.fillStyle = "aaaaaa";
+	context.fillRect( 0, 0, 500, 500);
+	//context.closePath();
+	context.lineWidth = 5;
+	context.font = "12px serif";
 	var i = 0;
 	for(i=0; i < numVertices; i++){
-		document.getElementById("statusdiv").innerHTML += grafos[i].nome;
+		//document.getElementById("statusdiv").innerHTML += grafos[i].nome + ", " + grafos[i].posx + ", " + grafos[i].posy;
+		context.beginPath();
+		context.fillStyle = "000000";
+		context.arc(grafos[i].posx - 10, grafos[i].posy - 102, 20, (Math.PI/180)*0, (Math.PI/180)*360, false); //
+		context.fill();
+		context.moveTo(grafos[i].posx - 10, grafos[i].posy - 102);
 		var j;
 		for(j=0; j < grafos[i].numlinks; j++){
-			document.getElementById("statusdiv").innerHTML += " -> " + grafos[i].linkpara[j].nome;
+			//document.getElementById("statusdiv").innerHTML += " -> " + grafos[i].linkpara[j].nome;
+			context.lineTo(grafos[grafos[i].linkpara[j].nome].posx - 10, grafos[grafos[i].linkpara[j].nome].posy - 102);
+			context.stroke();
+			
 		}
-		document.getElementById("statusdiv").innerHTML += "<br />";
+		context.fillStyle = "FFFFFF";
+		context.fillText(grafos[i].nome, grafos[i].posx - 13, grafos[i].posy - 97);
+		context.closePath();
+		//document.getElementById("statusdiv").innerHTML += "<br />";
 	}
+	;
 		
 }
-
+function deletar_vertice(){
+	action = 2;
+}
 function remover_vertice(){
+	action = 2;
 	var vertice = document.getElementById("verticedeletetext").value;
 
 	/*remover os links que chegam no vertice a ser deletado*/	
